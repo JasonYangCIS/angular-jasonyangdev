411310227a0bcd887d1d48b82d72a24793b27a78
Jason Yang <jasonyangcis@gmail.com>
2018-08-01 15:50:47 -0700

    finish angular tutorial for 'tour of heroes'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-jasonyangdev';
+  title = "Jason's tour of heroes";
 }
